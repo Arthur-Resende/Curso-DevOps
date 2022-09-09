1a2137b6e34eb41c0b7284221c77e43d3c605bce
Arthur-Resende <arthurresendefaria@gmail.com>
2022-09-09 12:07:25 -0300

    atividade 19 completa

---
+++ python/atividade1/script_19.py
@@ -0,0 +1,10 @@
+# Um professor quer sortear um dos seus quatro alunos para apagar o quadro. Faça um programa que ajude ele, lendo o nome dos alunos e escrevendo na tela o nome do escolhido.
+
+import random
+
+lista_alunos = []
+for i in range (0, 4):
+    lista_alunos.append(input("Nome aluno: "))
+
+aluno_escolhido = random.choice(lista_alunos)
+print("\nO aluno escolhido é: %s" % aluno_escolhido)
