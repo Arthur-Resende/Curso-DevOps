6c38d497ecf11245cc5fcab927c581161d8598b2
Arthur-Resende <arthurresendefaria@gmail.com>
2022-09-09 11:24:27 -0300

    atividade 14 feita

---
+++ python/atividade1/script_14.py
@@ -0,0 +1,4 @@
+# Escreva um programa que converta uma temperatura digitando em graus Celsius e converta para graus Fahrenheit.
+celsius = float(input("Temperatura em ˚C: "))
+fahrenheit = celsius * 1.8 + 32
+print(fahrenheit)
