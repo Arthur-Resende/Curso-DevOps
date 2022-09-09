27f220049c28590028693eabb4fb8bc5b799d66b
Arthur-Resende <arthurresendefaria@gmail.com>
2022-09-09 10:42:21 -0300

    novos scripts criados

--- python/atividade1/script_9.py
+++ python/atividade1/script_9.py
@@ -1,3 +1,3 @@
 numero = int(input("Digite um número para ver sua tabuada:"))
-for i in range(1, 10):
-    print("%d x %d = %d" %(numero, i, numero*i))
+for i in range(0, 10):
+    print("%d x %d = %d" %(numero, (i+1), numero*(i+1)))
