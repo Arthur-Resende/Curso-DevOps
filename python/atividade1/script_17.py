403713378da7b5b358f4f7522952c36813fa166e
Arthur-Resende <arthurresendefaria@gmail.com>
2022-09-09 12:06:33 -0300

    atividade 17 completa

--- python/atividade1/script_17.py
+++ python/atividade1/script_17.py
@@ -1,11 +1,11 @@
 # Um professor quer sortear um dos seus quatro alunos para apagar o quadro. 
 # Faça um programa que ajude ele, lendo o nome dos alunos e escrevendo na tela o nome do escolhido.
+
+import random
+
 lista_alunos = []
-continua = True
-while continua == True:
+for i in range (0, 4):
     lista_alunos.append(input("Nome aluno: "))
-    falta_aluno = input("falta aluno? s/n: ")
-    if falta_aluno == 's':
-        continue
-    elif falta_aluno == 'n':
-        break
+
+aluno_escolhido = random.choice(lista_alunos)
+print("O aluno escolhido é: %s" % aluno_escolhido)
