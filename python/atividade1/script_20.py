d338dab9b0a5b548cbdfbdd7d811639fb161c791
Arthur-Resende <arthurresendefaria@gmail.com>
2022-09-09 12:20:01 -0300

    Atividade 20 feita

---
+++ python/atividade1/script_20.py
@@ -0,0 +1,12 @@
+# O mesmo professor do exercício 19 quer sortear a ordem de apresentação de trabalhos dos alunos. 
+# Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada.
+
+import random
+
+lista_alunos = []
+for i in range (0, 4):
+    lista_alunos.append(input("Nome aluno: "))
+
+random.shuffle(lista_alunos)
+for i in range(0, len(lista_alunos)):
+    print("%d˚ %s" %(i+1, lista_alunos[i]))
