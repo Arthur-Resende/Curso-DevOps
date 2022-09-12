8a41d81fd25d16c3d2be4960312b4420278d4b2c
Arthur-Resende <arthurresendefaria@gmail.com>
2022-09-12 11:17:19 -0300

    atividade 25 adicionada

---
+++ python/atividade3/script_25.py
@@ -0,0 +1,14 @@
+# Crie um programa que leia o nome de uma pessoa e diga se ela tem "SILVA" no nome.
+nome = input("Nome: ").upper().strip()
+
+if nome.find("SILVA") != -1:
+    print("Seu nome tem Silva")
+    nome_destacado = []
+    for i in range(0, len(nome)):
+        if i >= nome.find("SILVA") and i < nome.find("SILVA") + 5:
+            nome_destacado.append(nome[i])
+        else:
+            nome_destacado.append(nome.lower()[i])
+    print("\n" + "".join(nome_destacado) + "\n")
+else:
+    print("Seu nome não tem Silva")
