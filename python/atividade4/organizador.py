939e99aec800250ba8e772dc30e16df983cf97d8
Arthur Resende Faria <arthurresendefaria@gmail.com>
2022-10-28 14:06:27 -0300

    .

--- python/atividade4/organizador.py
+++ python/atividade4/organizador.py
@@ -1,48 +1,21 @@
 import os
-
-file_types = {
-    "Text Files":['.DOC', '.DOCX', '.EML', '.LOG', '.MSG', '.ODT', '.PAGES', '.RTF', '.TEX', '.TXT', '.WPD'],
-    "Data Files":['.SDF', '.TAR', '.VCF', '.XML'],
-    "Audio Files":['.AIF', '.FLAC', '.M3U', '.M4A', '.MID', '.MP3', '.OGG', '.WAV', '.WMA'],
-    "Video Files":['.SWF', '.TS', '.VOB', '.WMV'],
-    "3D Image Files":['.3DM', '.3DS', '.BLEND', '.DAE', '.FBX', '.MAX', '.OBJ'],
-    "Raster Image Files":['.PNG', '.PSD', '.TGA', '.TIF'],
-    "Vector Image Files":['.AI', '.CDR', '.EMF', '.EPS', '.PS', '.SKETCH', '.SVG', '.VSDX'],
-    "Page Layout Files":['.AFPUB', '.INDD', '.OXPS', '.PDF', '.PMD', '.PUB', '.QXP', '.XPS'],
-    "Spreadsheet Files":['.NUMBERS', '.ODS', '.XLR', '.XLS', '.XLSX'],
-    "Database Files":['.PDB', '.SQL', '.SQLITE'],
-    "Executable Files":['.JAR', '.RUN', '.SH'],
-    "Game Files":['.BIN', '.DEM', '.GAM', '.GBA', '.NES', '.PAK', '.PKG', '.ROM', '.SAV', '.SAV'],
-    "CAD Files":['.DGN', '.DWG', '.DXF', '.STEP', '.STL', '.STP'],
-    "GIS Files":['.GPX', '.KML', '.KMZ', '.OSM'],
-    "Web Files":['.CSR', '.CSS', '.HTML', '.JS', '.JSON', '.JSP', '.PHP', '.XHTML'],
-    "Plugin Files":['.CRX', '.ECF', '.PLUGIN', '.SAFARIEXTZ', '.XPI'],
-    "Font Files":['.FNT', '.OTF', '.TTF', '.WOFF', '.WOFF2'],
-    "System Files":['.DLL', '.DMP', '.DRV', '.ICNS', '.ICO', '.LNK', '.REG', '.SYS'],
-    "Settings Files":['.CFG', '.INI', '.PKG', '.PRF', '.SET'],
-    "Encoded Files":['.ASC', '.BIN', '.ENC', '.MIM', '.UUE'],
-    "Compressed Files":['.PKG', '.RAR', '.RPM', '.gz">.TAR.GZ', '.XAPK', '.ZIP', '.ZIPX'],
-    "Disk Image Files":['.BIN', '.DMG', '.IMG', '.ISO', '.MDF', '.ROM', '.VCD', '.VMDK'],
-    "Developer Files":['.KT', '.LUA', '.M', '.MD', '.PL', '.PY', '.SB3', '.SLN', '.SWIFT', '.UNITY', '.VB', '.VCXPROJ', '.XCODEPROJ', '.YML'],
-    "Backup Files":['.ABK', '.ARC', '.BAK', '.TMP'],
-    "Misc Files":['.CRDOWNLOAD', '.ICS', '.MSI', '.NOMEDIA', '.PART', '.PKPASS', '.TORRENT'],
-    "Folders": ""
-}
+import file_types as file_type_dict
 
 def define_file_types():
-    download_files = os.scandir("/home/arthur/Downloads")
-    file_by_type = {}
-    for file in download_files:
-        file_ext = file.name[file.name.find('.'):].upper()
-        if file.is_dir() == False:
-            for i in file_types.keys():
-                if file_ext in file_types[i]:
-                    file_by_type[file.name] = i
+    download_file_list = os.scandir("/home/arthur/Downloads")
+    files_by_type = {}
+    for downloaded_file in download_file_list:
+        file_extention = downloaded_file.name[downloaded_file.name.find('.'):].upper()
+        if downloaded_file.is_dir() == False:
+            for file_type in file_type_dict.keys():
+                file_type_list = file_type_dict[file_type]
+                if file_extention in file_type_list:
+                    files_by_type[downloaded_file.name] = file_type
 
-        elif file.name not in file_types.keys():
-            file_by_type[file.name] = "Folders"
+        elif downloaded_file.name not in file_type_dict.keys():
+            files_by_type[downloaded_file.name] = "Folders"
 
-    return file_by_type
+    return files_by_type
 
 def create_dirs(files_by_type: dict):
     for file_type in files_by_type.items():
