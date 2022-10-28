8b9ae6d9ce23f42a33c93f27119e1416561acc4b
Arthur Resende Faria <arthurresendefaria@gmail.com>
2022-10-28 10:28:44 -0300

    função para criar pastas criada

--- python/atividade4/organizador.py
+++ python/atividade4/organizador.py
@@ -10,3 +10,19 @@ def define_tipo_arquivo():
         else:
             pastas.append(arquivo.name)
     return {"arquivos": arquivos, "pastas": pastas}
+
+def criar_pastas(arquivos_organizados: dict):
+    if arquivos_organizados["pastas"] != []:
+        try:
+            os.mkdir("/home/arthur/Downloads/pastas")
+        except:
+            print("pasta já existe")
+
+    if arquivos_organizados["arquivos"] != []:
+        try:
+            os.mkdir("/home/arthur/Downloads/arquivos")
+        except:
+            print("pasta já existe")
+
+arquivos_por_tipo = define_tipo_arquivo()
+criar_pastas(arquivos_por_tipo)
