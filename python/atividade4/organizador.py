6ac63c8c6da99cd03377456758890e0690462399
Arthur Resende Faria <arthurresendefaria@gmail.com>
2022-10-28 11:11:16 -0300

    código organizado

--- python/atividade4/organizador.py
+++ python/atividade4/organizador.py
@@ -1,37 +1,38 @@
 import os
 from unicodedata import name
 
-def define_tipo_arquivo():
-    arquivos = []
-    pastas = []
-    arquivos_downloads = os.scandir("/home/arthur/Downloads")
-    for arquivo in arquivos_downloads:
-        if arquivo.is_file() == True:
-            arquivos.append(arquivo.name)
-        elif arquivo.name != "pastas" and arquivo.name != "arquivos":
-            pastas.append(arquivo.name)
-    return {"arquivos": arquivos, "pastas": pastas}
+def define_file_types():
+    file_by_type = {"files": [], "dirs": []}
+    download_files = os.scandir("/home/arthur/Downloads")
+    for file in download_files:
+        if file.is_file() == True:
+            file_by_type["files"].append(file.name)
 
-def criar_pastas(arquivos_organizados: dict):
-    if arquivos_organizados["pastas"] != []:
+        elif file.name != "pastas" and file.name != "arquivos":
+            arquivos_por_tipo["dirs"].append(file.name)
+
+    return file_by_type
+
+def create_dirs(files_by_type: dict):
+    if files_by_type["dirs"] != []:
         try:
             os.mkdir("/home/arthur/Downloads/pastas")
         except:
-            print("pasta já existe")
+            pass
 
-    if arquivos_organizados["arquivos"] != []:
+    if files_by_type["files"] != []:
         try:
             os.mkdir("/home/arthur/Downloads/arquivos")
         except:
-            print("pasta já existe")
+            pass
 
-def mover_arquivos(caminho_base):
-    for i in arquivos_por_tipo["arquivos"]:
-        os.rename(caminho_base + i, caminho_base + "arquivos/" + i)
+def move_files(base_path, files_by_type: dict):
+    for i in files_by_type["files"]:
+        os.rename(base_path + i, base_path + "arquivos/" + i)
 
-    for i in arquivos_por_tipo["pastas"]:
-        os.rename(caminho_base + i, caminho_base + "pastas/" + i)
+    for i in files_by_type["dirs"]:
+        os.rename(base_path + i, base_path + "pastas/" + i)
 
-arquivos_por_tipo = define_tipo_arquivo()
-criar_pastas(arquivos_por_tipo)
-mover_arquivos("/home/arthur/Downloads/")
+arquivos_por_tipo = define_file_types()
+create_dirs(arquivos_por_tipo)
+move_files("/home/arthur/Downloads/", arquivos_por_tipo)
