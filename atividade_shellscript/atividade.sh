c852e6f6bf0b2fd78ef5a5c4128b6947be5c741a
Arthur Resende Faria <arthurresendefaria@gmail.com>
2022-11-18 16:35:37 -0300

    help flag added

--- atividade_shellscript/atividade.sh
+++ atividade_shellscript/atividade.sh
@@ -1,26 +1,23 @@
 #!/bin/bash
-# to-do
-# [ ] permitir coleta de dados de memória
-# [x] permitir o envio de dados a outro email e outro milestone, recebido por argumento
-# [x] criar inputs que determinam milestone e email caso parâmetros não sejam recebidos
-
 email=arthurresendefaria@gmail.com
 limit=90
 
-while getopts e:l: flag
+while getopts e:l:h flag
 do
     case "${flag}" in
         e) email="${OPTARG}";;
         l) limit="${OPTARG}";;
+        h) cat /home/arthur/Curso-DevOps/atividade_shellscript/help && exit 0;;
     esac
 done
 
 while true; do
-    cpu_use=(`sar 1 1 | head -n4 | tail -n1 | tr , .`)
-    cpu_use_total=`bc <<<"${cpu_use[2]} + ${cpu_use[3]} + ${cpu_use[4]}"`
+    cpu_use_raw=(`sar 1 1 | head -n4 | tail -n1 | tr , .`)
+    cpu_use_float=`bc <<<"100 - ${cpu_use_raw[7]}"`
+    cpu_use="${cpu_use_float%%.*}"
 
-    if [ ${cpu_use_total%%.*} -gt $limit ]; then
-        echo "Uso do cpu está em ${cpu_use_total}%, superando a marca de ${limit}%, alerta será enviado por email para ${email}."
+    if [ $cpu_use -gt $limit ]; then
+        echo "Uso do cpu está em ${cpu_use}%, superando a marca de ${limit}%, alerta será enviado por email para ${email}."
         echo "Alerta, o uso de cpu da máquina superou ${limit}%" | mail -s "Alerta" "${email}"
         echo "Email enviado"
         sleep 60
--- atividade_shellscript/teste.sh
+++
@@ -1,7 +0,0 @@
-while getopts a:b:c: flag; do
-    case "${flag}" in
-        a) a="${OPTARG}";echo "${a}";;
-        b) b="${OPTARG}";echo "${b}";;
-        c) c="${OPTARG}";echo "${c}";;
-    esac
-done
