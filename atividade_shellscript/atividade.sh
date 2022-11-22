fc0d2025b29a3d273aeec210dbf043d9cc04d022
Arthur Resende Faria <arthurresendefaria@gmail.com>
2022-11-22 16:50:18 -0300

    Forma a qual a lista é acessada alterada

--- atividade_shellscript/atividade.sh
+++ atividade_shellscript/atividade.sh
@@ -7,17 +7,16 @@ do
     case "${flag}" in
         e) email="${OPTARG}";;
         l) limit="${OPTARG}";;
-        h) cat /home/arthur/Curso-DevOps/atividade_shellscript/help && exit 0;;
+        h) cat ./help && exit 0;;
     esac
 done
 
 while true; do
-    cpu_use_raw=(`sar 1 1 | head -n4 | tail -n1 | tr , .`)
-    cpu_use_float=`bc <<<"100 - ${cpu_use_raw[7]}"`
-    cpu_use="${cpu_use_float%%.*}"
+    cpu_use_raw=`sar 1 1 | head -n4 | tail -n1 | tr , . | awk '{print $8}'`
+    cpu_use=`bc <<<"100 - ${cpu_use_raw}"`
 
-    if [ $cpu_use -gt $limit ]; then
-        echo "Uso do cpu está em ${cpu_use}%, superando a marca de ${limit}%, alerta será enviado por email para ${email}."
+    if [ "${cpu_use%%.*}" -ge $limit ]; then
+        echo "Uso do cpu está em ${cpu_use}%, sendo igual ou maior que a marca de ${limit}%, alerta será enviado por email para ${email}."
         echo "Alerta, o uso de cpu da máquina superou ${limit}%" | mail -s "Alerta" "${email}"
         echo "Email enviado"
         sleep 60
