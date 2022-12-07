9cc8ed16b61405fe0adf47939a07d0f790b1746f
Arthur Resende Faria <arthurresendefaria@gmail.com>
2022-12-07 16:15:58 -0300

    organized code

---
+++ shellscript/atividade_extra/collect_cpu_data.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+model=`cat /proc/cpuinfo | grep -i 'model name' | awk '{print $4" "$5" "$6" "$7" "$8" "$9}' | head -n1`
+cores=`cat /proc/cpuinfo | grep -i 'cpu cores' | awk '{print $4}' | head -n 1`
+threads=`cat /proc/cpuinfo | grep -i 'model name' | awk '{print $5" "$6" "$7" "$8" "$9}' | wc -l`
+cpu_gen=`echo "${model}" | head -n1 | awk '{print $3}' | cut -c 4-4`
+
+# Escreve dados
+printf "${BOLD}
+${WHITE}CPU
+
+${GREEN}Modelo da CPU      ${WHITE}------------------------- $model
+${GREEN}Geração            ${WHITE}------------------------- $cpu_genª geração
+${GREEN}Qntd de threads    ${WHITE}------------------------- $threads
+${GREEN}Qntd de núcleos    ${WHITE}------------------------- $cores
+\n\n"
---
+++ shellscript/atividade_extra/collect_mem_data.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+available_mem=`bc <<<"$(cat /proc/meminfo | grep -i memavailable | awk '{print $2}')/1024"`
+total_mem=`bc <<<"$(cat /proc/meminfo | grep -i memtotal | awk '{print $2}')/1024"`
+swap_total=`bc <<<"$(cat /proc/meminfo | grep -i swaptotal | awk '{print $2}')/1024"`
+swap_free=`bc <<<"$(cat /proc/meminfo | grep -i swapfree | awk '{print $2}')/1024"`
+
+# Escreve dados
+printf "${BOLD}
+${WHITE}MEMÓRIA
+
+${GREEN}Memória disponível ${WHITE}------------------------- $available_mem M
+${GREEN}Memória total      ${WHITE}------------------------- $total_mem M
+${GREEN}Swap total         ${WHITE}------------------------- $swap_total M
+${GREEN}Swap free          ${WHITE}------------------------- $swap_free M
+\n\n"
