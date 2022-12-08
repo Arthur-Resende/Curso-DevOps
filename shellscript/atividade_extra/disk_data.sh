03e0cd4678c88beaa3ee35b18c4851e457814048
Arthur-Resende <arthurresendefaria@gmail.com>
2022-12-08 17:05:37 -0300

    .

--- shellscript/atividade_extra/disk_data.sh
+++ shellscript/atividade_extra/disk_data.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # Coleta dados
-device_name=`df -H | grep -i sda2 | awk '{print $1}' | cut -c 6-9`
-disk_total=`df -H | grep -i sda2 | awk '{print $2}'`
-disk_used=`df -H | grep -i sda2 | awk '{print $3}'`
+device_name=`df -H | grep -i sda6 | awk '{print $1}' | cut -c 6-9`
+disk_total=`df -H | grep -i sda6 | awk '{print $2}'`
+disk_used=`df -H | grep -i sda6 | awk '{print $3}'`
+disk_free=`df -H | grep -i sda6 | awk '{print $4}'`
 
 # Escreve dados
 printf "${BOLD}
@@ -11,5 +12,6 @@ ${WHITE}ARMAZENAMENTO
 
 ${GREEN}Dispositivo        ${WHITE}------------------------- ${device_name}
 ${GREEN}Capacidade total   ${WHITE}------------------------- ${disk_total/G/ Gb}
-${GREEN}Capacidade usada   ${WHITE}------------------------- ${disk_used/G/ Gb}
+${GREEN}Espaço usada       ${WHITE}------------------------- ${disk_used/G/ Gb}
+${GREEN}Espaço liver       ${WHITE}------------------------- ${disk_free/G/ Gb}
 \n\n"
