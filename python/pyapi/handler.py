9626a002d30bdf02d6e0e22d84cd6d7be0fb8f27
arthur resende faria <arthurresendefaria@gmail.com>
2023-01-06 11:40:52 -0300

    usando match case, agora é possível determinar qual resposta é enviada para qual request

--- python/pyapi/handler.py
+++ python/pyapi/handler.py
@@ -1,12 +1,30 @@
+"""
+Serves responses to GET requests, base file in '/' and status codes in their respective paths
+(eg. '/200' or '/404')
+"""
 from http.server import HTTPServer, BaseHTTPRequestHandler
+from os import chdir
 
-class Handle_test(BaseHTTPRequestHandler):
+chdir("C:/Users/Arthur/Desktop/treinamento/python/pyapi")
+
+class Handler(BaseHTTPRequestHandler):
+    """
+    Instance of class BaseHTTPRequestHandler, since the class itself cannot respond to requests and
+    SimpleHTTPRequestHandler only serves files.
+    """
     def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-Type:", "text/plain; encoding: utf-8")
-        self.end_headers()
-        self.wfile()
+        """Responds to GET requests"""
+        match self.path:
+            case "/":
+                f = open("./index.html", 'r', encoding="utf-8")
+                self.wfile.write(f)
+
+            case "/200":
+                self.send_response(200)
+                self.send_header("Content-Type:", "text/plain; encoding: utf-8")
+                self.end_headers()
+                self.wfile.write(b'hello world')
 
 server_address = ('', 8000)
-httpd = HTTPServer(server_address, Handle_test)
+httpd = HTTPServer(server_address, Handler)
 httpd.serve_forever()
